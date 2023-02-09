6f547189cc42724fa14739a4d695e46d7672b71b
wojwen <wojwenc@gmail.com>
2023-02-09 12:13:38 +0100

    Simulate Boids

--- BoidSimulation/Assets/Scripts/BoidSimulation.cs
+++ BoidSimulation/Assets/Scripts/BoidSimulation.cs
@@ -12,6 +12,27 @@ public class BoidSimulation : MonoBehaviour, IDisposable
     /// <summary>The size of each batch of jobs.</summary>
     private const int JobBachSize = 32;
 
+    /// <summary>Maximum number of chunks within Boid's vision range.</summary>
+    public const int MaxNearbyChunkCount = 27;
+
+    /// <summary>Strength with which Boids try to avoid each other.</summary>
+    [SerializeField] private float separationStrength;
+
+    /// <summary>Strength with which Boids try to align their directions.</summary>
+    [SerializeField] private float alignmentStrength;
+
+    /// <summary>Strength with which Boids try to steer toward local group center.</summary>
+    [SerializeField] private float cohesionStrength;
+
+    /// <summary>Radius within which a Boid can see other Boids.</summary>
+    [SerializeField] private float visionRange;
+
+    /// <summary>Maximum velocity of a boid in m/s.</summary>
+    [SerializeField] private float maxVelocity;
+
+    /// <summary>Maximum velocity of a boid in m/s^2.</summary>
+    [SerializeField] private float maxAcceleration;
+
     /// <summary>Mesh used for rendering the Boids.</summary>
     [SerializeField] private Mesh boidMesh;
 
@@ -27,8 +48,13 @@ public class BoidSimulation : MonoBehaviour, IDisposable
     /// <summary>Number of boids in the simulation.</summary>
     [SerializeField] private int boidCount;
 
-    /// <summary>Native array containing the Boids</summary>
-    private NativeArray<Boid> _boids;
+    /// <summary>Native array containing the Boids at the current state, always swapped with <see cref="_boidsNext"/> at
+    /// the end of a simulation step.</summary>
+    private NativeArray<Boid> _boidsCurrent;
+
+    /// <summary>Native array for storing the new state of the Boids, always swapped with <see cref="_boidsCurrent"/> at
+    /// the end of a simulation step. Can be read from while the other array is being sorted.</summary>
+    private NativeArray<Boid> _boidsNext;
 
     /// <summary>Dimensions of each chunk.</summary>
     private Vector3Int _chunkDimensions;
@@ -58,16 +84,19 @@ public class BoidSimulation : MonoBehaviour, IDisposable
     {
         InitializeBoids();
         InitializeChunks();
+        _sortBoidsByChunksJob = _boidsCurrent.SortJob(new BoidChunkComparer(_chunkDimensions, chunkCount)).Schedule();
     }
 
     /// <summary>
     /// Runs the simulation. Steps of the simulation:
     /// <list>
-    /// <item>1. Initialize chunk start/end arrays.</item>
-    /// <item>2. Finding chunk start/end indexes.</item>
-    /// <item>3. Generate TRS matrices.</item>
-    /// <item>4. Draw all boids.</item>
-    /// <item>5. Sort Boids based on chunks.</item>
+    /// <item>1. Swap current and next Boid arrays.</item>
+    /// <item>2. Initialize chunk start/end arrays.</item>
+    /// <item>3. Finding chunk start/end indexes.</item>
+    /// <item>4. Simulate Boids.</item>
+    /// <item>5. Generate TRS matrices.</item>
+    /// <item>6. Draw all boids.</item>
+    /// <item>7. Sort Boids based on chunks.</item>
     /// </list>
     /// </summary>
     private void Update()
@@ -85,24 +114,46 @@ public class BoidSimulation : MonoBehaviour, IDisposable
             BoidCount = boidCount,
             ChunkDimensions = _chunkDimensions,
             ChunkCount = chunkCount,
-            Boids = _boids,
+            Boids = _boidsCurrent,
             ChunkStartIndexes = _chunkStartIndexes,
             ChunkEndIndexes = _chunkEndIndexes
         }.Schedule(boidCount, JobBachSize, initializeChunkArraysJob);
+
+        // simulate Boids
+        var simulateBoidsJob = new SimulateBoidsJob
+        {
+            SeparationStrength = separationStrength,
+            AlignmentStrength = alignmentStrength,
+            CohesionStrength = cohesionStrength,
+            VisionRange = visionRange,
+            SimulationDimensions = simulationDimensions,
+            ChunkCount = chunkCount,
+            ChunkDimensions = _chunkDimensions,
+            MaxAcceleration = maxAcceleration,
+            MaxVelocity = maxVelocity,
+            TimeDelta = Time.deltaTime,
+            BoidsCurrent = _boidsCurrent,
+            BoidsNext = _boidsNext,
+            ChunkStartIndexes = _chunkStartIndexes,
+            ChunkEndIndexes = _chunkEndIndexes
+        }.Schedule(boidCount, JobBachSize, findChunkBoundsJob);
 
         // schedule generating TRS matrices
         new GenerateTRSMatricesJob
         {
             SimulationOrigin = transform.position,
-            Boids = _boids,
+            Boids = _boidsNext,
             TRSMatrices = _trsMatrices
-        }.Schedule(boidCount, JobBachSize, findChunkBoundsJob).Complete();
+        }.Schedule(boidCount, JobBachSize, simulateBoidsJob).Complete();
         _trsMatrices.CopyTo(_trsMatricesHelperArray); // copy the TRS matrices to the helper array
 
         // draw the Boids in the simulation using the TRS matrices
         Graphics.DrawMeshInstanced(boidMesh, 0, boidMaterial, _trsMatricesHelperArray, boidCount);
 
-        _sortBoidsByChunksJob = _boids.SortJob(new BoidChunkComparer(_chunkDimensions, chunkCount)).Schedule();
+        (_boidsCurrent, _boidsNext) = (_boidsNext, _boidsCurrent);
+
+        // sort Boids in the new state based on their chunks
+        _sortBoidsByChunksJob = _boidsCurrent.SortJob(new BoidChunkComparer(_chunkDimensions, chunkCount)).Schedule();
     }
 
     /// <summary>
@@ -110,7 +161,8 @@ public class BoidSimulation : MonoBehaviour, IDisposable
     /// </summary>
     public void Dispose()
     {
-        _boids.Dispose();
+        _boidsCurrent.Dispose();
+        _boidsNext.Dispose();
         _trsMatrices.Dispose();
         _chunkStartIndexes.Dispose();
         _chunkEndIndexes.Dispose();
@@ -121,7 +173,8 @@ public class BoidSimulation : MonoBehaviour, IDisposable
     /// </summary>
     private void InitializeBoids()
     {
-        _boids = new NativeArray<Boid>(boidCount, Allocator.Persistent);
+        _boidsCurrent = new NativeArray<Boid>(boidCount, Allocator.Persistent);
+        _boidsNext = new NativeArray<Boid>(boidCount, Allocator.Persistent);
         _trsMatrices = new NativeArray<Matrix4x4>(boidCount, Allocator.Persistent);
         _trsMatricesHelperArray = new Matrix4x4[boidCount];
 
@@ -134,7 +187,8 @@ public class BoidSimulation : MonoBehaviour, IDisposable
                 z = Random.Range(0f, simulationDimensions.z)
             };
 
-            _boids[i] = new Boid { Position = randomPosition, Velocity = Random.insideUnitSphere };
+            _boidsCurrent[i] = new Boid { Position = randomPosition, Velocity = Random.insideUnitSphere };
+            _boidsNext[i] = new Boid { Position = randomPosition, Velocity = Random.insideUnitSphere };
         }
     }
 
@@ -159,6 +213,7 @@ public class BoidSimulation : MonoBehaviour, IDisposable
     /// <param name="chunkDimensions">Dimensions of each chunk.</param>
     public static int DetermineChunkId(Vector3 position, Vector3Int chunkCount, Vector3Int chunkDimensions)
     {
+        // the position can be cast to int since all chunks have integer dimensions
         var x = Mathf.Clamp((int)position.x / chunkDimensions.x, 0, chunkCount.x - 1);
         var y = Mathf.Clamp((int)position.y / chunkDimensions.y, 0, chunkCount.y - 1);
         var z = Mathf.Clamp((int)position.z / chunkDimensions.z, 0, chunkCount.z - 1);
---
+++ BoidSimulation/Assets/Scripts/SimulateBoidsJob.cs
@@ -0,0 +1,187 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+/// <summary>
+/// Simulates Boids based on the principles of separation, alignment and cohesion.
+/// </summary>
+[BurstCompile]
+public struct SimulateBoidsJob : IJobParallelFor
+{
+    /// <summary>Strength with which Boids try to avoid each other.</summary>
+    public float SeparationStrength;
+
+    /// <summary>Strength with which Boids try to align their directions.</summary>
+    public float AlignmentStrength;
+
+    /// <summary>Strength with which Boids try to steer toward local group center.</summary>
+    public float CohesionStrength;
+
+    /// <summary>Radius within which a Boid can see other Boids.</summary>
+    public float VisionRange;
+
+    /// <summary>Dimensions of the simulation the Boids are in.</summary>
+    public Vector3Int SimulationDimensions;
+
+    /// <summary>Number of chunks for each axis of the simulation.</summary>
+    public Vector3Int ChunkCount;
+
+    /// <summary>Dimensions of each chunk.</summary>
+    public Vector3Int ChunkDimensions;
+
+    /// <summary>Maximum velocity of a boid in m/s^2.</summary>
+    public float MaxAcceleration;
+
+    /// <summary>Maximum velocity of a boid in m/s.</summary>
+    public float MaxVelocity;
+
+    /// <summary>The interval in seconds from the last frame to the current one.</summary>
+    public float TimeDelta;
+
+    /// <summary>Read-only array of Boids to be simulated. Boids have to be sorted based on their chunks.</summary>
+    [ReadOnly] public NativeArray<Boid> BoidsCurrent;
+
+    /// <summary>Write-only array of Boids for storing simulated Boids.</summary>
+    [WriteOnly] public NativeArray<Boid> BoidsNext;
+
+    /// <summary>Read-only array for storing indexes of the first Boid in each chunk.</summary>
+    [ReadOnly] public NativeArray<int> ChunkStartIndexes;
+
+    /// <summary>Read-only array for storing indexes of the last Boid in each chunk.</summary>
+    [ReadOnly] public NativeArray<int> ChunkEndIndexes;
+
+    /// <summary>
+    /// Simulates a Boid based on the principles of separation, alignment and cohesion.
+    /// </summary>
+    /// <param name="index">The index of the Boid in the array.</param>
+    public void Execute(int index)
+    {
+        var boid = BoidsCurrent[index];
+
+        var outOfBoundsAcceleration = GetOutOfBoundsAccelerationDirection(boid.Position);
+
+        // if the boid was out of bounds accelerate back, otherwise accelerate in the simulated direction
+        var acceleration = outOfBoundsAcceleration == Vector3.zero
+            ? CalculateAccelerationDirection(index, boid) * MaxAcceleration
+            : outOfBoundsAcceleration * MaxAcceleration;
+
+        // calculate new velocity and clamp it
+        var newVelocity = boid.Velocity + acceleration * TimeDelta;
+        newVelocity = Vector3.ClampMagnitude(newVelocity, MaxVelocity);
+
+        // calculate new position based on new velocity
+        var newPosition = boid.Position + newVelocity * TimeDelta;
+
+        // save the calculated position and velocity
+        BoidsNext[index] = new Boid { Position = newPosition, Velocity = newVelocity };
+    }
+
+    /// <summary>
+    /// Calculates direction in which the Boid needs to accelerate to return to the simulation area when out of bounds.
+    /// </summary>
+    /// <param name="position">Position of the Boid.</param>
+    /// <returns>Direction towards the simulation area if the Boid is out of bounds, otherwise a zero vector.</returns>
+    private Vector3 GetOutOfBoundsAccelerationDirection(Vector3 position)
+    {
+        if (position.x > SimulationDimensions.x)
+            return -Vector3.right;
+        if (position.x < 0)
+            return Vector3.right;
+        if (position.y > SimulationDimensions.y)
+            return -Vector3.up;
+        if (position.y < 0)
+            return Vector3.up;
+        if (position.z > SimulationDimensions.z)
+            return -Vector3.forward;
+        if (position.z < 0)
+            return Vector3.forward;
+
+        return Vector3.zero;
+    }
+
+    /// <summary>
+    /// Calculates acceleration direction based on the principles of separation, alignment and cohesion.
+    /// </summary>
+    /// <param name="index">Index of the Boid in the array.</param>
+    /// <param name="boid">Boid which should be simulated.</param>
+    /// <returns>Direction vector for Boid's acceleration with magnitude proportional to acceleration.</returns>
+    private Vector3 CalculateAccelerationDirection(int index, Boid boid)
+    {
+        var nearbyChunkIds = GetNearbyChunkIds(boid.Position); // get chunks in vision range of the Boid
+
+        // initialize variables
+        var boidsInRange = 0;
+        var separationVector = Vector3.zero; // for storing combined direction pointing away from Boids in vision range
+        var averagePosition = Vector3.zero; // average position of Boids in vision range
+        var averageVelocity = Vector3.zero; // average velocity of Boids in vision range
+
+        // iterate over every chunk which contains Boids in vision range of the Boid
+        foreach (var chunkId in nearbyChunkIds)
+        {
+            var chunkStart = ChunkStartIndexes[chunkId]; // get the index of the first Boid in chunk
+            if (chunkStart == -1) continue; // skip the chunk if there are no Boids in it
+            var chunkEnd = ChunkEndIndexes[chunkId]; // get the index of the last Boid in chunk
+
+            // iterate over every Boid in chunk
+            for (var i = chunkStart; i <= chunkEnd; i++)
+            {
+                if (index == i) continue; // don't interact with itself
+
+                var otherBoid = BoidsCurrent[i];
+                var distance = Vector3.Distance(boid.Position, otherBoid.Position);
+                if (distance > VisionRange) continue; // skip this Boid if it's not in vision range
+
+                boidsInRange++;
+
+                // add to the separation vector the vector pointing away from the nearby boid with magnitude inversely
+                // proportional to the distance between them
+                separationVector += (VisionRange - distance) / VisionRange *
+                                    (boid.Position - otherBoid.Position).normalized;
+                averagePosition += otherBoid.Position;
+                averageVelocity += otherBoid.Velocity;
+            }
+        }
+
+        // if there are no Boids in range then don't change direction
+        if (boidsInRange == 0)
+            return boid.Velocity.normalized;
+
+        separationVector /= boidsInRange; // calculate the average separation vector
+        averagePosition /= boidsInRange; // calculate the average nearby Boid position
+        averageVelocity /= boidsInRange; // calculate the average nearby Boid velocity
+
+        // calculate the cohesion vector as the direction toward the average Boid position with normalized magnitude
+        var cohesionVector = (averagePosition - boid.Position) / VisionRange;
+
+        // calculate te alignment as the average normalized velocity of nearby Boids 
+        var alignmentVector = averageVelocity / MaxVelocity;
+
+        // calculate the weighted average of separation, alignment and cohesion vectors
+        return (separationVector * SeparationStrength + cohesionVector * CohesionStrength +
+                alignmentVector * AlignmentStrength) / (SeparationStrength + CohesionStrength + AlignmentStrength);
+    }
+
+    /// <summary>
+    /// Finds IDs of chunks which are within vision range of a Boid.
+    /// </summary>
+    /// <param name="position">Position of the Boid.</param>
+    /// <returns>Hash set containing the chunk IDs.</returns>
+    private NativeHashSet<int> GetNearbyChunkIds(Vector3 position)
+    {
+        // allocate hash set with Temp allocator since it will be used within the job
+        var neighbourChunks = new NativeHashSet<int>(BoidSimulation.MaxNearbyChunkCount, Allocator.Temp);
+
+        // find chunks which are within a cube encapsulating the vision radius of the Boid
+        for (var x = -VisionRange; x <= VisionRange; x += VisionRange)
+        for (var y = -VisionRange; y <= VisionRange; y += VisionRange)
+        for (var z = -VisionRange; z <= VisionRange; z += VisionRange)
+        {
+            // when determining chunk Ids the position is clamped so there is no need to check if it's out of bounds
+            var edgePosition = position + new Vector3(x, y, x);
+            neighbourChunks.Add(BoidSimulation.DetermineChunkId(edgePosition, ChunkCount, ChunkDimensions));
+        }
+
+        return neighbourChunks;
+    }
+}
